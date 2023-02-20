c55320e305ddc7d9cf2aea2cc6ad7802786364c9
AdamTempest <jj030469@gmail.com>
2023-02-20 16:32:19 +0630

    fixed bugs

--- Grab Free Proxies/crawler.py
+++ Grab Free Proxies/crawler.py
@@ -2,9 +2,15 @@ import logging
 import requests
 from os import system
 from bs4 import BeautifulSoup
+from random import choice, randint
 from urllib.parse import urljoin
 
 logging.basicConfig(format='%(asctime)s %(levelname)s:%(message)s',level=logging.INFO)
+
+proxyList = {
+    'http': {},
+    'https':{}
+}
 
 # See if the row has ip address or not
 def isProxyData(row):
@@ -21,32 +27,72 @@ def getThecontent(tag):
     i = 0
     while tag[i] != '>':
         i+=1
-    return tag[i+1:-5] # <td>
+    return tag[i+1:-5]
+
+def getProxy():
+    proxies = {
+    'http':'',
+    'https':''}
+    http_country = choice([i for i in proxyList['http'].keys()])
+    https_country = choice(list(proxyList['https'].keys()))
+
+    http_length = len(proxyList['http'][http_country])
+    https_length = len(proxyList['https'][https_country])
+
+    http_max = http_length-1 if http_length>0 else http_length
+    https_max = https_length-1 if https_length>0 else https_length
+
+    proxies['http'] = proxyList['http'][http_country][randint(0,http_max)]
+    proxies['https'] = proxyList['https'][https_country][randint(0,https_max)]
+
+    return proxies
+
+
+# read file with the following format: https http://125.125.125.125:80 UA
+def addToProxyList(filename):
+    with open(filename,'r') as f:
+        data = f.readlines()
+    
+    for i in range(0,len(data)):
+        l = data[i].split()
+        try:
+            proxyList[l[0]][l[2]].append(l[1])
+        except KeyError as error:
+            proxyList[l[0]][l[2]] = [l[1]]
+        # ['http'][LocationCode].append('http://ip:port')
+        # LocationCode examples: UA, RU, NL, FR etc
+
 
 # main function
 def crawl(url):
-    column = [] # ip, port, code, country, provider, google, https, other
-    html = requests.get(url).text  # download_url
-    soup = BeautifulSoup(html, 'html.parser')
+    proxies=getProxy()
+    logging.info(f'Using {proxies} to crawl {url}')
+    html = requests.get(url, proxies=proxies, timeout=3).text  # download_url
     
-    with open('proxy.list','a') as f:
-        logging.info(f'Initiated database: proxy.list')
+    logging.info(f'Successfully crawled {url} using {proxies}')
+
+    soup = BeautifulSoup(html, 'html.parser')
+
+    file = 'proxies_Fried_With_proxies'
+    with open(file,'a') as f:
+        logging.info(f'Initiated the database: {file}')
         for data in soup.find_all('tr'):
+            column = [] # ip, port, code, country, provider, google, https, other
             if (isProxyData(str(data))):
                 for col in data.find_all('td'):
                     content = getThecontent(str(col))
                     column.append(content)
-                    logging.info(f'Adding to db: {content}')
-                f.write(f'{column[6]} http://{column[0]}:{column[1]}')
-                f.write('\n')
-            else:
-                pass
+                    
+                logging.info(f'Cleaning the data: {column}')
+                head = "https" if column[6] == "yes" else "http"
+                f.write(f'{head} http://{column[0]}:{column[1]} {column[2]}\n')
 
 
 if __name__ == '__main__':
-    url = 'https://free-proxy-list.net/'
-    logging.info(f'Crawling: {url}')
     try:
+        url = 'https://free-proxy-list.net/'
+        logging.info(f'Crawling: {url}')
+        addToProxyList("proxies_Fried_With_proxies")
         crawl(url)
     except Exception:
         logging.exception(f'Failed to crawl: {url}')
--- Using proxy to send requests/anonymous crawler.py
+++ Using proxy to send requests/anonymous crawler.py
@@ -7,14 +7,17 @@ from urllib.parse import urljoin
 logging.basicConfig(format='%(asctime)s %(levelname)s:%(message)s',level=logging.INFO)
 
 proxies = {
-   'http': 'http://proxy.example.com:8080',
-   'http': 'http://ip:port',
    'http': 'http://10.10.10.10:8000',
-   'https': 'http://secureproxy.example.com:8090',
+   'https': 'http://secureproxy.example.com:8090'
 }
 
 def readDB(filename):
-	with open(filename, )
+   cleaned = []
+   with open(filename, 'r') as f:
+      data = f.readlines()
+   for i in range(0,len(data)):
+      cleaned.append(data[i][:-2].split(':'))
+   
 
 def crawl(url):
 	html = requests.get(url, proxies=proxies).text
